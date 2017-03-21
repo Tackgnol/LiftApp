cfc9e73ec53a552bb6735bfbceb2e4bd10d24b2a
Adam <koscielniakadam@gmail.com>
2017-03-21 12:24:59 +0100

    Final Model1

---
+++ LiftApp/LiftApp/LiftApp/Models/WorkOutType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiftApp.Models
+{
+    class WorkOutType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ LiftApp/LiftApp/LiftApp/Models/Workout.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiftApp.Models
+{
+    class Workout
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Comments { get; set; }
+        public string ImageDirectory { get; set; }
+        public virtual WorkOutType Type { get; set; }
+        public virtual ICollection<Exercise> Exercises { get; set; }
+    }
+}
